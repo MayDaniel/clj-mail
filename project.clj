20d3da9a0966597ee3a3906e11dc38d7f20c47ff
MayDaniel <yamnad1@gmail.com>
2010-06-27 14:49:54 +0100

    Clj-Mail

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject clj-mail "0.1.0"
-  :description "FIXME: write"
+  :description "Send and receive emails from Clojure."
   :repositories {"java-net" "http://download.java.net/maven/2"}
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [javax.mail/mail "1.4.3"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
-										 [lein-clojars "0.5.0"]]
+                     [lein-clojars "0.5.0"]]
   :namespaces :all)
