ed984509fa72c6913363479c583d054cfc97b666
MayDaniel <yamnad1@gmail.com>
2010-08-28 19:30:20 +0100

    Updated to 1.2.0

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject clj-mail "0.1.1"
+(defproject clj-mail "0.1.2"
   :description "Send and receive emails from Clojure."
   :repositories {"java-net" "http://download.java.net/maven/2"}
-  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
-                 [org.clojure/clojure-contrib "1.2.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [javax.mail/mail "1.4.3"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
-                     [lein-clojars "0.5.0"]]
-  :namespaces [clj-mail.core])
+                     [lein-clojars "0.5.0"]])
