2b10430b70237528cffa61c33a170eb10003bdcc
MayDaniel <MayDaniel95@gmail.com>
2012-02-05 14:25:18 +0000

    .

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
-(defproject clj-mail "0.1.5"
+(defproject clj-mail "0.1.6"
   :description "Send and receive emails from Clojure."
   :repositories {"java-net" "http://download.java.net/maven/2"}
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [javax.mail/mail "1.4.3"]]
-  :dev-dependencies [[lein-clojars "0.5.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [javax.mail/mail "1.4.3"]]])
